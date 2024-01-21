54093913bbbb481d00a5df8e25af7e1555d46f73
Vishal Parmar <94777911+Vishal9098@users.noreply.github.com>
2024-01-21 18:20:52 +0530

    Time: 7 ms (98.23%), Space: 19.3 MB (20.63%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,20 +2,11 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        int size = nums.size();
-        if(nums.size() == 1)
-            return nums[0];
+        int ans = 0;
         
-        sort(nums.begin(), nums.end());
-        
-        if(nums[0] != nums[1])
-            return nums[0];
-        
-        for(int i = 1; i < size; i++) {
-                
-                if(nums[i] != nums[i+1] && nums[i] != nums[i-1])
-                    return nums[i];
-            }
-        return nums[size];
+        for(int i = 0; i < nums.size(); i++)
+            ans = ans ^ nums[i];
+    
+        return ans;
     }
 };
