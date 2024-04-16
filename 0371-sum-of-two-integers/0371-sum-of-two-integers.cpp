78e303d6dbfcdb6a7dbd04d1ef8b26bcfbadfd29
Vishal Parmar <94777911+vishalparmarr@users.noreply.github.com>
2024-04-16 22:37:38 +0530

    Time: 0 ms (100.00%), Space: 7 MB (94.41%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        
+        if(b > 0) {
+        for(int i = 0; i < b; i++) {
+            a++;
+            cout << a << " ";
+        }
+        }
+        else {
+            for(int i = 0; i > b; i--) {
+                a--;
+            }
+        }
+        return a;
     }
 };
