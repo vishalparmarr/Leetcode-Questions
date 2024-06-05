dc22385e69a95a2f81d27fda9bdd3b53103a390e
Vishal Parmar <94777911+vishalparmarr@users.noreply.github.com>
2024-06-05 23:36:43 +0530

    Time: 6 ms (30.53%), Space: 11.4 MB (11.49%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,14 +4,13 @@ public:
         
         int start = 0;
         int end = nums.size() - 1;
-        int ans = 0;
         int mid = start + (end - start) / 2;
         
         while(start < end) {
             
-            if(nums[mid] < nums[mid + 1]) {
+            if(nums[mid] < nums[mid + 1])
                 start = mid + 1;
-            }
+            
             else
                 end = mid;
             
