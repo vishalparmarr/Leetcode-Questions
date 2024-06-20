a397f6f9bbd8067a5c87eea362fd78b0e85be8f9
Vishal Parmar <94777911+vishalparmarr@users.noreply.github.com>
2024-06-20 21:40:56 +0530

    Time: 6 ms (21.60%), Space: 7.9 MB (32.38%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != t[i]) 
+                return t[i];
+        }
+        return t[t.size() - 1];
+    }
+};
