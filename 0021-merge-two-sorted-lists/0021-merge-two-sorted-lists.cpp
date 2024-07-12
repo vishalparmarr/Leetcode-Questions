8ec6822f7e114bf925c0526fa2ff9464c22e1ab6
Vishal Parmar <94777911+vishalparmarr@users.noreply.github.com>
2024-07-12 18:26:19 +0530

    Time: 3 ms (81.95%), Space: 19.7 MB (8.51%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,59 +10,43 @@
  */
 class Solution {
 public:
-    
-    void insertAtTail(ListNode* &tail, int d) {
-    
-        ListNode* temp = new ListNode(d);
-        tail -> next = temp;
-        tail  = temp;
-}
-    
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
-        ListNode* list3 = NULL;
-        
-        if(list1 == NULL && list2 == NULL)
-            return NULL;
-        
-        if(list1 == NULL && list2 != NULL)
+        if(list1 == nullptr)
             return list2;
         
-        if(list1 != NULL && list2 == NULL)
+        if(list2 == nullptr)
             return list1;
         
-        if(list1 -> val <= list2 -> val) {
-                list3 = new ListNode(list1 -> val);
-                list1 = list1 -> next;    
-        }
-        else {
-            list3 = new ListNode(list2 -> val);
-            list2 = list2 -> next;
-        }
+        ListNode* ans = new ListNode(-1);
+        ListNode* temp = ans;
         
-        ListNode* tail = list3;
-        ListNode* head = list3;
-        while(list1 != NULL && list2 != NULL) {
-            
-            if(list1 -> val <= list2 -> val) {
-                insertAtTail(tail, list1 -> val);
-                list1 = list1 -> next;
-            }
-            else {
-                insertAtTail(tail, list2 -> val);
-                list2 = list2 -> next;
-            }
-        }
-        
-        while(list1 != NULL) {
-            insertAtTail(tail, list1 -> val);
+        while(list1 != nullptr && list2 != nullptr) {
+
+        if(list1 -> val < list2 -> val) {
+            temp -> next = list1;
+            temp = list1;
             list1 = list1 -> next;
         }
-        
-        while(list2 != NULL) {
-            insertAtTail(tail, list2 -> val);
-            list2 = list2 -> next;
+        else {
+            temp -> next = list2;
+            temp = list2;
+            list2 = list2 -> next; 
         }
-        return head;
+    }
+
+    while(list1 != NULL) {
+        temp -> next = list1;
+        temp = list1;
+        list1 = list1 -> next;
+    }
+
+    while(list2 != NULL) {
+        temp -> next = list2;
+        temp = list2;
+        list2 = list2 -> next; 
+    }
+        
+    return ans -> next;
     }
 };
